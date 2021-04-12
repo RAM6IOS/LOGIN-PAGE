58cb89b3ed3c8aa5db093e842176cb7abe0b9d71
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-04-12 22:10:00 +0100

    Initial Commit

---
+++ Login Page/Login_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_PageApp.swift
+//  Login Page
+//
+//  Created by MAC on 12/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct Login_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
